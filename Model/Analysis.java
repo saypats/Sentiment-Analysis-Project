ccf2c61ae7b6d1ef65d5c75f788c5830adf6904f
saypats <48220340+saypats@users.noreply.github.com>
2019-08-05 03:37:11 -0500

    Update Analysis.java

--- Model/Analysis.java
+++ Model/Analysis.java
@@ -1,19 +1,27 @@
 package Model;
 
 import java.util.*;
+import View.Review_Form;
 import java.io.*;
 import java.math.BigDecimal;
 import java.net.URISyntaxException;
 import java.net.URL;
 
 public class Analysis {
-  
-  
+	
+	//Global result-variables to be displayed to User
+	public double accuracy = 0.0;
+	public static int sentiment = 0;	 //1 is Positive, -1 is negative 
+	public String reviewDecision = "";
+	//public double polarity = 0.0;
+	public double precision = 0.0;
+	//public double movieRating = 0.0;
+				
   //probability tables
   static private Map<String,Double> wordProbs = new HashMap<String,Double>();
   static private Set<String> dictionary = new HashSet<String>();
   
-  //static getter methods
+  //static getter methods to get: WordProbability and Dictionary
   static public Map<String,Double> getWordProbs(){
     return wordProbs;
   }
@@ -21,71 +29,47 @@ public class Analysis {
     return dictionary;
   }
   
-  //Makes Dictionary. No Negation.
-  static private void makeDictionary(String myDirectoryPathPos, String myDirectoryPathNeg)throws IOException, URISyntaxException{
-    Set<String> blacklist = new HashSet<String>(Arrays.asList(new String[] {"you","josh","!josh","games","!games","katniss","!jennifer","jennifer","lawrence","mocking","!mocking","watching","!watching","got","whole","!whole","having",".","to","!to","n't","can","!can","then","there","this","!this","!then","be","!be","else","!else","so","!so","i","!i","movie","!movie","they","!they","is","!is","was","!was","were","!were"}));
-    List<List<String>> posReviews=readReviewData(myDirectoryPathPos);
-    for(List<String> review : posReviews){      
-      for(String word : review){
-        dictionary.add(word.toLowerCase());  
-      }
-    }
-    List<List<String>> negReviews=readReviewData(myDirectoryPathNeg);
-    for(List<String> review : negReviews){     
-      for(String word : review){
-        dictionary.add(word.toLowerCase());  
-      }
-    }
-    for(String word : blacklist){
-      dictionary.remove(word);
-    }
-  }
-  
+  /**
+   * Training the Algorithm: Creating dictionary from all possible reviews.
+   */
   //Makes Dictionary. Negation.
-  static private void makeDictionaryNegation(String testPos, String testNeg)throws IOException, URISyntaxException{
-    Set<String> blacklist = new HashSet<String>(Arrays.asList(new String[] {"games","!games","katniss","!Jennifer","Jennifer","Lawrence","mocking","!mocking","watching","!watching","got","whole","!whole","having",".","to","!to","n't","can","!can","then","there","this","!this","!then","be","!be","else","!else","so","!so","i","!i","movie","!movie","they","!they","is","!is","was","!was","were","!were"}));
-    List<List<String>> posReviews=readReviewDataNegation(testPos);
-    for(List<String> review : posReviews){      
-      for(String word : review){
-        dictionary.add(word.toLowerCase());  
-      }
-    }
-    List<List<String>> negReviews=readReviewDataNegation(testNeg);
-    for(List<String> review : negReviews){     
-      for(String word : review){
-        dictionary.add(word.toLowerCase());  
-      }
-    }
-    for(String word : blacklist){
-      dictionary.remove(word);
-    }
-  }
-  
-  //Turns all the reviews inside a directory into a list of lists of words. No Negation.
-  static private List<List<String>> readReviewData(String myDirectoryPathPos)throws IOException, URISyntaxException{
-    List<List<String>> reviews = new LinkedList<List<String>>();
-    
-    URL path = ClassLoader.getSystemResource(myDirectoryPathPos);
-    File dir = new File(path.toURI());
-    
-    File[] directoryListing = dir.listFiles();
-    if (directoryListing != null) {
-      for (File child : directoryListing) {
-        String stringReview = fileToString(child);
-        //Tokenizer tokenizer = new Tokenizer();
-        //reviews.add(tokenizer.tokenize(stringReview));
-        StringTokenizer tokenizer = new StringTokenizer(stringReview);
-        reviews.add((List<String>) tokenizer.nextElement());
-      }
-    }
-    return reviews;
-  }
-  
-  //Turns all the reviews inside a directory into a list of lists of words. Negation.
-  static private List<List<String>> readReviewDataNegation(String testPos)throws IOException, URISyntaxException{
+  static private void makeDictionaryNegationDatasets(String testPos, String testNeg, String stopwords)throws IOException, URISyntaxException{
+	  
+	 
+	  //Read "stop-words" file and save it as a SET.
+	  URL stopPath = ClassLoader.getSystemResource(stopwords);
+	    File dir = new File(stopPath.toURI());
+	    
+	    Set<String> blacklist = new HashSet<String>(Arrays.asList(new String[] {"games","!games","katniss","!Jennifer",
+	    		"Jennifer","Lawrence","mocking","!mocking","watching","!watching","got","whole","!whole","having",".","to",
+	    		"!to","n't","can","!can","then","there","this","!this","!then","be","!be","else","!else","so",
+	    		"!so","i","!i","movie","!movie","they","!they","is","!is","was","!was","were","!were"}));
+	    
+	    //Get words from positive reviews' folder
+	    List<List<String>> allReviewsWords = readReviewDataNegation(testPos);
+	    for(List<String> review : allReviewsWords){      
+	      for(String word : review){
+	        dictionary.add(word.toLowerCase());  
+	      }
+	    }
+	  //Get words from negative reviews' folder
+	    List<List<String>> negReviews = readReviewDataNegation(testNeg);
+	    for(List<String> review : negReviews){     
+	      for(String word : review){
+	        dictionary.add(word.toLowerCase());  
+	      }
+	    }    
+	    //remove stop-words from dictionary (Blacklisted words)
+	    for(String word : blacklist){
+		      dictionary.remove(word); 
+	    }
+	  }
+
+  //Turns all the reviews inside a directory into a list of "lists of words". Includes Negation.
+  static private List<List<String>> readReviewDataNegation(String testPath)throws IOException, URISyntaxException{
     List<List<String>> reviews = new LinkedList<List<String>>();  
     
-    URL path = ClassLoader.getSystemResource(testPos);
+    URL path = ClassLoader.getSystemResource(testPath);
     File dir = new File(path.toURI());
     
     File[] directoryListing = dir.listFiles();
@@ -96,10 +80,12 @@ public class Analysis {
         reviews.add(tokenizer.tokenize(stringReview));
       }
     }
+    System.out.print("List of 'List of words': " + reviews);
     return reviews;
   }
   
-  //Reads files. Used by readReviewData & readReviewDataNegation
+
+  //Reads a file. Used by readReviewDataNegation
   static private String fileToString(File file) throws IOException{
     StringBuilder fileContents = new StringBuilder((int)file.length());
     Scanner scanner = new Scanner(file);
@@ -114,8 +100,8 @@ public class Analysis {
     }
   }
   
-  //Naiive Bayes Classifier
-  static private void train(List<List<String>> reviews, int sentiment){
+  //Naive-Bayes-Classifier: Algorithm that classifies the review. Used for training purpose
+  static private void train(List<List<String>> reviews, int movieRating){
     Map<String,Integer> wordCounts = new HashMap<String,Integer>();
     for(String word: dictionary)
     {
@@ -130,9 +116,14 @@ public class Analysis {
           wordCounts.put(word.toLowerCase(),wcount+1);     
       }
     }
-    // Compute observation probabilities                
+    // Compute observation probabilities  
+    if(movieRating > 5)
+		sentiment = 1;
+	else if (movieRating < 5) 
+		sentiment = -1;
+    
     for(String word : dictionary){
-      double prob = (double)wordCounts.get(word)/(tokenCount+dictionary.size());
+    	double prob = (double)wordCounts.get(word)/(tokenCount+dictionary.size());
       if(sentiment==1){
         wordProbs.put(word+"^+",prob);
       }
@@ -142,8 +133,9 @@ public class Analysis {
     }
   }
   
-  //Tests reviews. Sentiment is 1 for postive, -1 for negative.
-  static private double test(List<List<String>> reviews, int sentiment){
+  
+  //Tests reviews. Sentiment is 1 for positive, -1 for negative.
+  static private double test(List<List<String>> reviews, int movieRating){
     int reviewCount = 0;
     int correctReviewCount = 0;
     BigDecimal pos= new BigDecimal("1.0");
@@ -158,6 +150,13 @@ public class Analysis {
           neg=neg.multiply(probneg);
         }
       }
+      
+     //Setting sentiment according to movie ratings
+     if(movieRating > 5)
+  		sentiment = 1;
+     else if (movieRating < 5) 
+  		sentiment = -1;
+      
       if(pos.compareTo(neg)>0 && sentiment==1){
         correctReviewCount++;
       }
@@ -167,13 +166,62 @@ public class Analysis {
       pos= new BigDecimal("1.0");
       neg= new BigDecimal("1.0");
     }
-    System.out.println(correctReviewCount+" " +reviewCount);        
+    System.out.println("CorrectReviewCount:"+correctReviewCount+"   " +"Review Count:"+reviewCount);        
     double correctProb=(double)correctReviewCount/(double)reviewCount;
     return correctProb;
   }  
   
   
+  /* *******  ADDED MODEL GETTER SETTER FUNCTIONS ******* */
+  // dummy data to make sure it is getting sent from Model to the View, via Controller
+  public String getReviewDecision(int rating) {
+	  if(rating > 5)
+	  		sentiment = 1;
+	     else if (rating < 5) 
+	  		sentiment = -1;
+	  
+	  if(sentiment == 1)	
+		  reviewDecision = "Positive";
+	  else
+		  reviewDecision = "Negative";  
+	  return reviewDecision;
+  }
   
+  // dummy data to make sure it is getting sent from Model to the View, via Controller      
+  public double getAccuracy() {
+         return accuracy;
+  }
+  
+  //*** These functions are just to make sure the info is being passed to the Model
+  // function just to make sure that the user's review is making it to the Model...
+  public void printReview(String uReview) {
+         System.out.println("This is your Review: " + uReview);
+  }
+  
+  public void printPath(String p) {
+	   System.out.println("This is the path for the file selected: " + p);
+  }
+  
+public void setMovieRating(int rating) {
+	
+}
+
+
+public void testSingle(String uR, int rating) {
+	if(rating > 5) {
+		
+	}
+	
+	else if(rating <5) {
+		
+		
+	}
+	
+}
+  
+  
+  
+  /*
   public static void main(String[] args) throws IOException, URISyntaxException {
     
 	  System.out.println("Enter TRAIN directories: POSITIVE...");
@@ -181,28 +229,16 @@ public class Analysis {
 	  String trainPos = train.next();
 	  System.out.println("Enter TRAIN directories: NEGATIVE...");
 	  String trainNeg = train.next();
+	  System.out.println("Enter path to Stopwords list...");
+	  String stopPath = train.next();
 	  
 	  System.out.println("\nEnter TEST directories: POSITIVE...");
 	  Scanner test = new Scanner(System.in);
 	  String testPos = test.next();
 	  System.out.println("Enter TEST directories: NEGATIVE...");
 	  String testNeg = test.next();
-	  
-	  //String trainFile = "Data/Train.data", testFile = "Data/Test_1.data", vocabFile = "Data/Vocab.data", stopwordFile = "Data/stopwords.txt";
-		
-//		URL path1 = ClassLoader.getSystemResource(trainPos);
-//		File trainP = new File(path1.toURI());
-//		
-//		URL path2 = ClassLoader.getSystemResource(trainNeg);
-//		File trainN = new File(path2.toURI());
-//		
-//		URL path3 = ClassLoader.getSystemResource(testPos);
-//		File testP = new File(path3.toURI());
-//		
-//		URL path4 = ClassLoader.getSystemResource(testNeg);
-//		File testN = new File(path4.toURI());
-//	  
-		makeDictionaryNegation(testPos, testNeg);
+  
+		makeDictionaryNegationDatasets(testPos, testNeg, stopPath);
 		
 		List<List<String>> trainingReviewsPos = readReviewDataNegation(trainPos);
 	    List<List<String>> trainingReviewsNeg = readReviewDataNegation(trainNeg);   
@@ -212,37 +248,8 @@ public class Analysis {
 	    
 	    List<List<String>> testReviewsPos = readReviewDataNegation(testPos);
 	    List<List<String>> testReviewsNeg = readReviewDataNegation(testNeg);
+	        
 	    
-	    
-//	  
-//	  if(args.length != 4){
-//      System.err.println("Usage: java <Positive_Train_Directory> <Negative_Train_Directory> <Positive_Test_Directory> <Negative_Test_Directory>");
-//      System.exit(1);
-//    }    
-	    
-// makeDictionaryNegation(args[2],args[3]);	
-//    List<List<String>> trainingReviewsPos = readReviewDataNegation(args[0]);
-//    List<List<String>> trainingReviewsNeg = readReviewDataNegation(args[1]);    
-   // train(trainingReviewsPos,1);
-    //train(trainingReviewsNeg,-1);
-    /*for(String word: dictionary){
-     System.out.println(word+"^+: "+wordProbs.get(word+"^+"));
-     System.out.println(word+"^-: "+wordProbs.get(word+"^-"));
-     }*/
-//    List<List<String>> testReviewsPos = readReviewDataNegation(args[2]);
-//    List<List<String>> testReviewsNeg = readReviewDataNegation(args[3]);
-    
-    /*for(List<String> review : testReviewsPos){
-     for(String word : review){
-     System.out.println(wordProbs.get(word.toLowerCase()+"^+"));
-     }
-     }
-     System.out.println();
-     for(List<String> review : testReviewsPos){
-     for(String word : review){
-     System.out.println(wordProbs.get(word.toLowerCase()+"^-"));
-     }
-     }*/
     System.out.println("Positive Reviews:");
     double posProb=test(testReviewsPos,1);
     System.out.println("Negative Reviews:");
@@ -251,4 +258,5 @@ public class Analysis {
     System.out.println("Negative Review Accuracy: " + negProb);
     System.out.println("Overall Accuracy: " + (double)Math.round(((posProb+negProb)/2.0)* 1000)/1000);                     
   }
+  */
 }
