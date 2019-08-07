66e81bc407e33f65803a4b3253f50e8241cdba42
saypats <48220340+saypats@users.noreply.github.com>
2019-08-06 23:58:52 -0500

    Update Controller.java

--- Controller/Controller.java
+++ Controller/Controller.java
@@ -3,6 +3,8 @@ package Controller;
 import Model.Analysis;
 import View.Review_Form;
 import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
  
@@ -16,7 +18,7 @@ public class Controller {
        public Controller(Review_Form theView, Analysis theModel) {
     	   this.theView = theView;
     	   this.theModel = theModel;
-    	   this.theView.addTesterListener(new addTesterListener());
+    	   //this.theView.addTesterListener(new addTesterListener());
     	   this.theView.addSubmitListener(new addSubmitListener());
     	   this.theView.addRunListener(new addRunButton());
        }
@@ -24,30 +26,45 @@ public class Controller {
        // This Listener is only temporary, will need to be removed when adding the Model
      public class addTesterListener implements ActionListener{
               public void actionPerformed(ActionEvent e) {
-                     int rating = theView.getRating();
+                     int rating = Integer.parseInt(theView.getRating());
 					theView.setTypeOfReview(theModel.getReviewDecision(rating));
-                     theView.setAccuracy(theModel.getAccuracy());
-                     //theView.setPolarity(theModel.getPolarity());
-                     //theView.setMovieRating(theModel.getMovieRating());
+                    theView.setAccuracy(theModel.getAccuracy());
+                    
               }
        }
       
-       // This listener will need to add commands to send results to the UI, 
-       //	similar to tester up above
+      //Listener for Submit button
        class addSubmitListener implements ActionListener{
               public void actionPerformed(ActionEvent e) {
                      String uR = theView.getUserReview();
-                     int rating = theView.getRating();
-                     theModel.testSingle(uR, rating);
+                     int rating = Integer.parseInt(theView.getRating());
+                     try {
+						theModel.single(uR, rating);
+					} catch (IOException e1) {
+						System.out.print("Error occurred");
+						e1.printStackTrace();
+					} catch (URISyntaxException e1) {
+						System.out.print("Error occurred");
+						e1.printStackTrace();
+					}
               }
        }
        
-       // This listener will also need to add commands to send results to the UI,
-       //	similar to tester up above
+     //Listener for Run Button
        class addRunButton implements ActionListener{
     	   public void actionPerformed(ActionEvent e) {
-    		   String path = theView.getUploadPath();
-    		   theModel.printPath(path);
+    		   String pathPos = theView.getPath_pos();
+    		   String pathNeg = theView.getPath_neg();
+    		   double rating = Double.parseDouble(theView.getRating());
+    		   try {
+				theModel.dataset(pathPos, pathNeg, rating);
+			} catch (IOException e1) {
+				System.out.print("Error occurred");
+				e1.printStackTrace();
+			} catch (URISyntaxException e1) {
+				System.out.print("Error occurred");
+				e1.printStackTrace();
+			}
     	   }
        }
  
